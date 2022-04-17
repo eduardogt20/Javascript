c6bf5be2ddd57fdcb4957a53e56aee02464bdfa7
Bairon Garita <Bairongaba@gmail.com>
2022-04-17 15:14:45 -0600

    Bases de javascript

---
+++ 1.Bases/Variables.js
@@ -0,0 +1,12 @@
+/*
+ * Tipos de Datos:
+ * undefined, null, boolean, string, symbol, number, y object.
+ */
+
+var miNombre = "Bairon";
+
+console.log(miVariable);
+
+miVariable = 16;
+
+console.log(miVariable);
